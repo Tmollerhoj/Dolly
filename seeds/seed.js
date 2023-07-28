e61bc621b9a4785561201c1b789307bac497e0e4
Adam <adamgjohnson92@gmail.com>
2023-07-27 21:12:32 -0700

    Fixed seed organization for testing

--- seeds/seed.js
+++ seeds/seed.js
@@ -8,34 +8,27 @@ const commentData = require('./commentData.json');
 const followerData = require('./followerData.json');
 
 const seedDatabase = async () => {
-    await sequelize.sync({ force: true });
-  
-    const users = await User.bulkCreate(userData, {
+  await sequelize.sync({ force: true });
+
+  for (const userDataItem of userData) {
+
+    await User.create(userDataItem, {
       individualHooks: true,
       returning: true,
     });
-  
-    for (const bleet of bleetData) {
-      await Bleet.create({
-        ...bleet,
-        user_id: users[Math.floor(Math.random() * users.length)].id,
-      });
-    }
-  
-    for (const comment of commentData) {
-      await Comment.create({
-        ...comment,
-        user_id: users[Math.floor(Math.random() * users.length)].id,
-      });
-    }
-    for (const follower of followerData) {
-        await Follower.create({
-          ...follower,
-          user_id: users[Math.floor(Math.random() * users.length)].id,
-        });
-      }
-  
-    process.exit(0);
-  };
+  }
+  await Bleet.bulkCreate(bleetData, {
+    returning: true,
+  });
 
-  seedDatabase();
+  await Comment.bulkCreate(commentData, {
+    returning: true,
+  });
+  await Follower.bulkCreate(followerData, {
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+seedDatabase();
