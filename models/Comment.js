cf39c8f627670fb93824014ea87d23054aea7f49
tmollerhoj <tmollerhoj@gmail.com>
2023-07-27 19:10:18 -0700

    finished models

--- models/Comment.js
+++ models/Comment.js
@@ -4,7 +4,49 @@ const sequelize = require('../config/connection');
 class Comment extends Model {}
 
 Comment.init (
-
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        content: {
+            type: DataTypes.STRING,
+        },
+        likes: {
+            type: DataTypes.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        bleet_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'bleet',
+                key: 'id',
+            },
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment',
+    },
+    
 );
 
 module.exports = Comment;
--- models/Follower.js
+++ models/Follower.js
@@ -1,2 +1,31 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Follower extends Model {}
+
+Follower.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+        follower_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            },
+        },
+    }
+)
+
+module.exports = Follower;
